5adae8d83b6dc71c76aaef1e8fd25b370660cf5e
Shevilll <ahmadfaraz00710@gmail.com>
2025-02-01 18:08:49 +0530

    fixed centering issue

--- src/pages/Motto.jsx
+++ src/pages/Motto.jsx
@@ -6,10 +6,10 @@ const Motto = () => {
             <p className="text-[48px]">Motto</p>
             <p className=" w-[120px] border border-[#FFCC00] relative mb-[30px]"></p>
             <div className="w-full h-auto border border-black p-[30px] flex flex-col justify-center items-center">
-                <p className="text-[22px] font-light">
+                <p className="text-[22px] font-light text-center">
                     The motto of National Service Scheme is
                 </p>
-                <p className="text-[40px]">NOT ME BUT YOU.</p>
+                <p className="text-[40px] text-center">NOT ME BUT YOU.</p>
             </div>
         </div>
     );
