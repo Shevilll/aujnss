837f4d735045093a63387d4fc8cb75f1e630c833
Shevilll <ahmadfaraz00710@gmail.com>
2025-06-20 23:31:28 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="h-[60px] w-full bg-[#1C2025] flex items-center justify-center z-10">
-            <p className="text-[#D4DAE3] text-center px-[10px]">
-                Copyright © 2025 - Amity University Jhakhand - All Rights
-                Reserved
-            </p>
-        </div>
-    );
+  return (
+    <div className="h-[80px] w-full bg-[#1C2025] flex items-center justify-center z-10 flex-col">
+      <p className="text-[#D4DAE3] text-center px-[10px]">
+        Copyright © 2025 - Amity University Jhakhand - All Rights Reserved
+      </p>
+      <p className="text-[#D4DAE3] text-center px-[10px]">
+        Website Designed & Developed by{" "}
+        <a
+          href="https://theahmadfaraz.com"
+          className="underline"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Ahmad Faraz
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
